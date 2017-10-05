cbb4bf175e40b864175d6f6d53f1a68c0e8fd71e
Akash Kumar Seth <kumarakashseth007@gmail.com>
2017-10-05 13:00:35 +0530

    practising with ES6 Arrow Function.

---
+++ src/playground/es6-arrow-function.js
@@ -0,0 +1,15 @@
+const square = function(x){
+    return x * x;
+};
+
+// const squareArrow = (x) =>{
+//   return x * x;
+// };
+
+//consize version
+const squareArrow = (x) => x * x;
+console.log(squareArrow(7));
+
+//Practise
+const firstName = (fullName) => fullName.split(' ')[0];
+console.log(firstName("Mike Hussey"));
