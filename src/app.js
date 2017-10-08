f3fb61b5227dd22236c9c93dcb0f73bf8653e39c
Akash Kumar Seth <kumarakashseth007@gmail.com>
2017-10-09 01:02:32 +0530

    Added counter App code to counter-example.js file in playground.

--- src/app.js
+++ src/app.js
@@ -21,36 +21,5 @@ const template = (
     </div>
 );
 
-//event and attributes a simple counter app.
-let count = 0;
-
-const addOne = () => {
-  count++;
-  renderCounterApp();
-};
-
-const minusOne = () => {
-    count--;
-    renderCounterApp();
-};
-
-const reset = () => {
-    count = 0;
-    renderCounterApp();
-};
-
-const appRoot = document.getElementById('app');
-
-const renderCounterApp = () => {
-    const templateTwo = (
-        <div>
-            <h1>Count: {count}</h1>
-            <button onClick={addOne}>+1</button>
-            <button onClick={minusOne}>-1</button>
-            <button onClick={reset}>reset</button>
-        </div>
-    );
-    ReactDOM.render(templateTwo,appRoot);
-};
-
-renderCounterApp();
+appRoot = document.getElementById('app');
+ReactDOM.render(template, appRoot);
