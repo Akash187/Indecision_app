19266abbe248640fa57be2e9e5640d9d4dec6155
Akash Kumar Seth <kumarakashseth007@gmail.com>
2017-10-28 12:37:25 +0530

    Lecture 29 Events and Methods in src/app.js

--- src/app.js
+++ src/app.js
@@ -28,19 +28,26 @@ class Header extends React.Component {
 }
 
 class Action extends React.Component {
+    handlePick(){
+        alert('handle pick');
+    }
     render() {
         return (
             <div>
-                <button>What should I do?</button>
+                <button onClick={this.handlePick}>What should I do?</button>
             </div>
         );
     }
 }
 
 class Options extends React.Component {
+    handleRemoveAll(){
+        alert('RemoveAll clicked');
+    }
     render() {
         return (
             <div>
+                <button onClick={this.handleRemoveAll}>Remove All</button>
                 {
                     //key is a special name
                     this.props.options.map((option) => <Option key={option} optionText={option}/>)
@@ -62,12 +69,24 @@ class Option extends React.Component {
 }
 
 class AddOptions extends React.Component {
-    render() {
+    handleAddOptions(e) {
+        e.preventDefault();
+
+        const option = e.target.elements.option.value.trim();
+
+        if (option) {
+        alert('AddOption : ' + option);
+        }
+    }
+    render(){
         return (
             <div>
-                <h4>AddOption component here</h4>
+                <form onSubmit={this.handleAddOptions}>
+                    <input type = "text" name="option"/>
+                    <button>Add Option</button>
+                </form>
             </div>
-        );
+        )
     }
 }
 
