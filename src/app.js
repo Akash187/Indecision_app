7adb8409e33e90f4183ef32f4c6cbc04f458a46c
Akash Kumar Seth <kumarakashseth007@gmail.com>
2017-11-09 23:20:24 +0530

    Lecture 53 Importing npm modules

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
-//import './utils';
-import isSeniorCitizen, { square, add, isAdult, canDrink } from './utils';
-console.log('app.js is running!!');
-console.log(square(4));
-console.log(add(123, 34));
-console.log(isAdult(17));
-console.log(canDrink(20));
-console.log(isSeniorCitizen(69));
+//install -> import -> use
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const template = React.createElement('p',{},'akash123');
+ReactDOM.render(template,document.getElementById('app'));
+
+import validator from 'validator';
+console.log('First : ' + validator.isEmail('akash@gmail.com'));
+
+//import only a subset of the library
+import isEmail from 'validator/lib/isEmail';
+console.log('Second : ' + isEmail('seth@gmail.com'));
