aad196b7d5d874e361c93486906bb29a54cb4ff2
Akash Kumar Seth <kumarakashseth007@gmail.com>
2017-11-09 09:03:39 +0530

    Lecture 52 default export.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 //import './utils';
-import{ square, add, isAdult, canDrink } from './utils';
+import isSeniorCitizen, { square, add, isAdult, canDrink } from './utils';
 console.log('app.js is running!!');
 console.log(square(4));
 console.log(add(123, 34));
 console.log(isAdult(17));
 console.log(canDrink(20));
+console.log(isSeniorCitizen(69));
--- src/utils.js
+++ src/utils.js
@@ -2,8 +2,9 @@ const square = (x) => x*x;
 const add = (a,b) => a+b;
 const isAdult = (age) => age >= 18;
 const canDrink = (age) => age >= 21;
+const isSenior = (age) => age >= 65;
 
-//named export
-export { square, add, isAdult, canDrink };
+//named export and default export
+export { square, add, isAdult, canDrink, isSenior as default };
 
 console.log('Utils.js is running');
