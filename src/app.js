ba358be637e91eb4b59826ba015cf7db4785c46c
Akash Kumar Seth <kumarakashseth007@gmail.com>
2017-11-28 05:20:16 +0530

    Lecture 65,66 Header Style and Reset default browser setting.

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
+import 'normalize.css/normalize.css';
 import './styles/main.scss';
 
 ReactDOM.render(<IndecisionApp/>, document.getElementById('app'));
