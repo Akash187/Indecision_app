c186772073277efef7da5ae82a69c5ad97d1e59e
Akash Kumar Seth <kumarakashseth007@gmail.com>
2018-01-31 03:26:06 +0530

    Lecture 72 Finish Styling.

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -82,15 +82,17 @@ export default class IndecisionApp extends React.Component {
             hasOptions={this.state.options.length > 0}
             handlePick={this.handlePick}
           />
-          <Options
-            options={this.state.options}
-            handleDeleteOptions={this.handleDeleteOptions}
-            hasOptions={this.state.options.length > 0}
-            handleDeleteOption={this.handleDeleteOption}
-          />
-          <AddOption
-            handleAddOption={this.handleAddOption}
-          />
+          <div className="widget">
+            <Options
+              options={this.state.options}
+              handleDeleteOptions={this.handleDeleteOptions}
+              hasOptions={this.state.options.length > 0}
+              handleDeleteOption={this.handleDeleteOption}
+            />
+            <AddOption
+              handleAddOption={this.handleAddOption}
+            />
+          </div>
         </div>
         <OptionModal
           selectedOption={this.state.selectedOption}
--- src/components/Options.js
+++ src/components/Options.js
@@ -2,17 +2,29 @@ import React from 'react';
 import Option from './Option';
 
 const Options = (props) => (
-    <div>
-        <button onClick={props.handleDeleteOptions} disabled={!props.hasOptions}>Remove All</button>
-        {props.options.length === 0 && <p>Please Enter an option to get Started!</p>}
-        {
-            //key is a special name
-            props.options.map((option) => (
-                <Option key={option} optionText={option}
-                        handleDeleteOption={props.handleDeleteOption}/>
-            ))
-        }
+  <div>
+    <div className="widget-header">
+      <h3 className="widget-header__title">Your Options</h3>
+      <button
+        onClick={props.handleDeleteOptions}
+        disabled={!props.hasOptions}
+        className="button button--link">
+        Remove All
+      </button>
     </div>
+    {props.options.length === 0 && <p className="widget__message">Please Enter an option to get Started!</p>}
+    {
+      //key is a special name
+      props.options.map((option, index) => (
+        <Option
+          key={option}
+          optionText={option}
+          count = {index + 1}
+          handleDeleteOption={props.handleDeleteOption}
+        />
+      ))
+    }
+  </div>
 );
 
 export default Options;
