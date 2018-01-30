3fb91b110c967af0ebf26db45fb46fe1f1a9e42a
Akash Kumar Seth <kumarakashseth007@gmail.com>
2018-01-31 00:06:12 +0530

    Lecture 67 Theming with Variables. Added OptionModal file to git repositiory.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,17 @@
 import React from 'react';
 
 const Header = (props) => (
-    <div className={"header"}>
-        <h1 className={"header__title"}>{props.title}</h1>
-        {props.subtitle && <h2 className={"header__subtitle"}>{props.subtitle}</h2>}
+  <div className={"header"}>
+    <div className="container">
+      <h1 className={"header__title"}>{props.title}</h1>
+      {props.subtitle && <h2 className={"header__subtitle"}>{props.subtitle}</h2>}
     </div>
+  </div>
 );
 
 //default props
 Header.defaultProps = {
-    title: 'Indecision'
+  title: 'Indecision'
 };
 
 export default Header;
--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -6,82 +6,97 @@ import Action from './Action';
 import OptionModal from './OptionModal';
 
 export default class IndecisionApp extends React.Component {
-    state = {
-        options: [],
-        selectedOption: undefined
-    };
-    //lifecycle method
-    componentDidMount() {
-        try {
-            const json = localStorage.getItem('options');
-            const options = JSON.parse(json);
-            if (options) {
-                this.setState(() => ({options}));
-            }
-        }
-        catch (e) {
-            //Do Nothing at all
-        }
+  state = {
+    options: [],
+    selectedOption: undefined
+  };
+
+  //lifecycle method
+  componentDidMount() {
+    try {
+      const json = localStorage.getItem('options');
+      const options = JSON.parse(json);
+      if (options) {
+        this.setState(() => ({options}));
+      }
     }
-
-    componentDidUpdate(prevProps, prevState) {
-        if (prevState.options.length !== this.state.options.length) {
-            const json = JSON.stringify(this.state.options);
-            localStorage.setItem('options', json);
-        }
+    catch (e) {
+      //Do Nothing at all
     }
+  }
 
-
-    //implicitely returning an object
-    handleDeleteOptions = () => {
-        this.setState(() => ({options: []}));
-    };
-
-    //code to remove Individual option
-    handleDeleteOption = (optionToRemove) => {
-        this.setState((prevState) => ({
-            options: prevState.options.filter((option) =>
-                optionToRemove !== option)
-        }));
-    };
-
-    handlePick = () => {
-        const randomNumber = Math.floor(Math.random() * this.state.options.length);
-        const option = this.state.options[randomNumber];
-        this.setState(() =>({
-            selectedOption: option
-        }));
-    };
-
-    handleAddOption = (option) => {
-        if (!option) {
-            return 'Enter valid value to add item';
-        } else if (this.state.options.indexOf(option) > -1) {
-            return 'This option already exists';
-        }
-        //converted from explicit to implicit return
-        this.setState((prevState) => ({
-            options: prevState.options.concat([option])
-        }));
-    };
-
-    handleClearModal = () => {
-        this.setState(() => ({
-            selectedOption: undefined
-        }));
-    };
-
-    render() {
-        const subtitle = 'Put your life in the hand of a Computer.';
-        return (
-            <div>
-                <Header subtitle={subtitle}/>
-                <Action hasOptions={this.state.options.length > 0} handlePick={this.handlePick}/>
-                <Options options={this.state.options} handleDeleteOptions={this.handleDeleteOptions}
-                         hasOptions={this.state.options.length > 0} handleDeleteOption={this.handleDeleteOption}/>
-                <AddOption handleAddOption={this.handleAddOption}/>
-                <OptionModal selectedOption={this.state.selectedOption} handleClearModal = {this.handleClearModal}/>
-            </div>
-        );
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.options.length !== this.state.options.length) {
+      const json = JSON.stringify(this.state.options);
+      localStorage.setItem('options', json);
     }
+  }
+
+
+  //implicitely returning an object
+  handleDeleteOptions = () => {
+    this.setState(() => ({options: []}));
+  };
+
+  //code to remove Individual option
+  handleDeleteOption = (optionToRemove) => {
+    this.setState((prevState) => ({
+      options: prevState.options.filter((option) =>
+        optionToRemove !== option)
+    }));
+  };
+
+  handlePick = () => {
+    const randomNumber = Math.floor(Math.random() * this.state.options.length);
+    const option = this.state.options[randomNumber];
+    this.setState(() => ({
+      selectedOption: option
+    }));
+  };
+
+  handleAddOption = (option) => {
+    if (!option) {
+      return 'Enter valid value to add item';
+    } else if (this.state.options.indexOf(option) > -1) {
+      return 'This option already exists';
+    }
+    //converted from explicit to implicit return
+    this.setState((prevState) => ({
+      options: prevState.options.concat([option])
+    }));
+  };
+
+  handleClearModal = () => {
+    this.setState(() => ({
+      selectedOption: undefined
+    }));
+  };
+
+  render() {
+    const subtitle = 'Put your life in the hand of a Computer.';
+    return (
+      <div>
+        <Header subtitle={subtitle}/>
+        <div className="container">
+          <Action
+            hasOptions={this.state.options.length > 0}
+            handlePick={this.handlePick}
+          />
+          <Options
+            options={this.state.options}
+            handleDeleteOptions={this.handleDeleteOptions}
+            hasOptions={this.state.options.length > 0}
+            handleDeleteOption={this.handleDeleteOption}
+          />
+          <AddOption
+            handleAddOption={this.handleAddOption}
+          />
+        </div>
+        <OptionModal
+          selectedOption={this.state.selectedOption}
+          handleClearModal={this.handleClearModal}
+        />
+      </div>
+    );
+  }
 }
